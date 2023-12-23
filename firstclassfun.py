ab18292e6ba7824a090a7382faad8ea42ad5e145
MRDK369 <contactme2486@gmail.com>
2023-12-23 17:28:23 +0530

    classfuntion

---
+++ firstclassfun.py
@@ -0,0 +1,11 @@
+def create_adder(x):
+    def adder(y):
+        return(x+y)
+    def subtractory(y):
+        return(x-y)
+    return adder, subtractory
+
+
+add_5 , sub_5 = create_adder(40)
+print(add_5(100))
+print(sub_5(5))
